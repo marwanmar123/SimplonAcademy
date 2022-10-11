0fa411f99a7404d6cd4d383ea7a65ebee2c588c6
Maryo <youcode098@gmail.com>
2022-10-11 17:09:37 +0100

    chnages

---
+++ SimplonAcademy/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿
+
+//DataTable
+
+$(document).ready(function () {
+    $("#myTable").DataTable();
+})
+//EndDataTable
+
+
+
+/**
+ * Mobile nav toggle
+ */
+on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+})
+
+/**
+ * Mobile nav dropdowns activate
+ */
+on('click', '.navbar .dropdown > a', function (e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+        e.preventDefault()
+        this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+}, true)
+
